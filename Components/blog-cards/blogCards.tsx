5a9083d4f9637ee4e0a501733830b1ebab4d1bce
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-08-10 21:34:45 -0500

    more dynamic routing tweaks

--- Components/blog-cards/blogCards.tsx
+++ Components/blog-cards/blogCards.tsx
@@ -1,29 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 
-
-interface blogTestId {
-    id: number
-}
 
 export const BlogCards: React.FC = () => {
-    
-const blogTestId: blogTestId = {
-    id: 37
-}
+    const router = useRouter()
+    const { id } = router.query
+  
 
 
 
-
-    return(
-        <div className="container mx-auto pt-16">
-<div className="mb-10">
-    <h1 className="xl:text-5xl pt-4 xl:pt-0 text-3xl text-gray-800 text-center font-extrabold mb-4">Our Blog Posts</h1>
-    <p className="text-xl text-gray-600 text-center xl:w-3/5 mx-auto w-11/12">
-        Don&apos;t focus on having a great blog. Focus on producing a blog that’s great for your readers.” As a blogger, everything you do flows from understanding your audience and seeking to help them.
-    </p>
-</div>
-<div className="lg:flex md:flex xl:justify-around sm:flex flex-wrap md:justify-around sm:justify-around lg:justify-around">
+return(
+<div className="container mx-auto pt-16">
+    <div className="mb-10">
+        <h1 className="xl:text-5xl pt-4 xl:pt-0 text-3xl text-gray-800 text-center font-extrabold mb-4">Our Blog Posts</h1>
+        <p className="text-xl text-gray-600 text-center xl:w-3/5 mx-auto w-11/12">
+         Don&apos;t focus on having a great blog. Focus on producing a blog that’s great for your readers.” As a blogger, everything you do flows from understanding your audience and seeking to help them.
+        </p>
+    </div>
+    <div className="lg:flex md:flex xl:justify-around sm:flex flex-wrap md:justify-around sm:justify-around lg:justify-around">
     <div className="xl:w-1/3 sm:w-5/12 sm:max-w-xs relative mb-32 lg:mb-20 xl:max-w-sm lg:w-1/2 w-11/12 mx-auto sm:mx-0">
         <div className="shadow h-64 rounded z-10">
             <Image 
@@ -43,7 +39,12 @@ const blogTestId: blogTestId = {
     </div>
     <div className="xl:w-1/3 sm:w-5/12 sm:max-w-xs relative mb-32 lg:mb-20 xl:max-w-sm lg:w-1/2 w-11/12 mx-auto sm:mx-0">
         <div className="shadow h-64 rounded">
-            <a href={`/blog/${blogTestId.id}`} className="href">
+            <Link 
+            href='/blog/[id]'
+            as={`/blog/${id}`} 
+            className="href"
+            >
+            <a>
             <Image 
             src="https://cdn.tuk.dev/assets/photo-1544476915-ed1370594142.jfif" 
             alt="STUFF" 
@@ -51,9 +52,10 @@ const blogTestId: blogTestId = {
             layout='fill'
             />
             </a>
+            </Link>
         </div>
         <div className="p-6 shadow-lg w-11/12 mx-auto -mt-20 bg-white rounded z-20 relative">
-            <p className="uppercase text-sm text-indigo-700 text-center pb-3">Work</p>
+            <p className="uppercase text-sm text-indigo-700 text-center pb-3">Work {id}</p>
             <p className="text-lg text-gray-800 text-center pb-3">I&apos;m a greater believer in luck, and I find the harder I work the more I have of it.</p>
             <p className="text-sm text-gray-800 text-center">
                 May 13, 2019 by <a href="javascript:void(0)"><span className="text-indigo-700 cursor-pointer">Sansa Rollins</span></a>
@@ -77,7 +79,7 @@ const blogTestId: blogTestId = {
             </p>
         </div>
     </div>
-</div>
+    </div>
 </div>
  )
 }
--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+
+
 
 module.exports = {
   images: {
     domains: ['cdn.tuk.dev'],
   },
-  nextConfig
 }
