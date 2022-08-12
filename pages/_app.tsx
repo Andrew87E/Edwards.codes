8471d69b2a01a93c3fd9b7e22670c891840c7a7f
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-08-11 19:10:48 -0500

    tailwind fix &#8482;

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import "../styles/global.scss";
+import '../build.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
