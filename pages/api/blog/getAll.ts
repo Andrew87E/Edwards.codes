44c6712c1a9c9708ff9bc6e49bf8b87d0f9485f7
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-08-12 23:54:51 -0500

    fixed middleware bugs

--- pages/api/blog/getAll.ts
+++ pages/api/blog/getAll.ts
@@ -1,6 +1,5 @@
 import connectMongo from "../../../utils/connectMongo";
 import Blog from "../../../models/blogPosts";
-import colors from "colors";
 
 /**
  * @param {import('next').NextApiRequest} req
@@ -11,11 +10,10 @@ export default async function getBlog(req: any, res: any) {
     try {
         console.log('GETALL.TS')
         console.log("CONNECTING TO MONGO");
-        
         await connectMongo();
         console.log("CONNECTED TO MONGO!");
 
-        console.log("GETTING DOCUMENT".red);
+        console.log("GETTING DOCUMENT");
         const blog = await Blog.find()
 
         res.status(200).json(blog)
