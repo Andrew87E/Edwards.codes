e661c9bb3a6161f8c13bf8da2e05270258b43708
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-08-13 16:46:13 -0500

    landing page finished

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,8 +8,8 @@ const Home: GetStaticProps = () => {
 
   return (
     <>
+      <ParticlesContainer />
       <Page currentPage="Home" meta={{ desc: "I'm a passionate developer interested in coding in every language" }}>
-      {/* <ParticlesContainer /> */}
       <Jumbotron />
       <Intro />
       <Hero />
