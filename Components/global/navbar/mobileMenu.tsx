05d3b301a6d9b4d1d980d17efddf6bbeb71b7833
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-08-13 20:41:31 -0500

    mobile nav bar finished

--- Components/global/navbar/mobileMenu.tsx
+++ Components/global/navbar/mobileMenu.tsx
@@ -2,25 +2,44 @@ import Link from "next/link";
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import {routes} from "../../../data/global"
-import { HiOutlineMenuAlt3, HiOutlineX } from "react-icons/hi";
+import { HiOutlineX } from "react-icons/hi";
+import { RiMenu3Line } from 'react-icons/ri'
 import { Name } from "./name";
 
 
 export const MobileMenu = ({currentPage}: any) => {
 const [showMenu, setShowMenu] = useState(false)
 
+
+const handleClick = () => {
+    setShowMenu(current => !current)
+}
+
     return (
         <>
         <Name />
+        <button className="mr-4 text-3xl transition-all transition-transform duration-200"
+        onClick={handleClick}
+        >
+         {showMenu 
+          ?<HiOutlineX 
+           color="white"
+          />   
+          :<RiMenu3Line 
+          color="white"
+          />
+            }
+        </button>
 
         {showMenu ?
-        <ul className="list-none inline-block mt-80 ae-mobile-menu border-t border-b">
+        <ul className="list-none inline-block fixed right-0 top-16 ae-mobile-menu border-t border-b w-36 h-60"> 
+        {/* // use position not mt */}
         {
             routes.map((item, index)=>{
               return (
                 <li
                 key={index}
-                className={`mr-5 hover:border-gray-300 transition-all duration-700 hover:duration-100 hover:scale-105 h-10 p-1 align-center
+                className={`mr-8 text-2xl hover:border-gray-300 transition-all duration-700 hover:duration-100 hover:scale-105 h-10 p-1 align-center
                 ${
                   currentPage === item.title
                   ? "text-lime-500 hover:text-white border-b"
