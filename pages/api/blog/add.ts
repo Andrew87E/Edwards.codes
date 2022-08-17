5a89c4d59f3ac3bba279e975f814270e9d70e3c7
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-08-16 21:44:04 -0500

    fixed api route

--- pages/api/blog/add.ts
+++ pages/api/blog/add.ts
@@ -9,19 +9,22 @@ import Blog from "../../../models/blogPosts";
 export default async function addBlog(req: any, res: any) {
     try {
         console.log("CONNECTING TO MONGO");
+
         await connectMongo();
+
         console.log("CONNECTED TO MONGO");
 
         console.log("CREATING DOCUMENT");
-        const blog = await Blog.create(req.body);
-        console.log("CREATED DOCUMENT");
 
-        res.json({ blog });
+        const blog = await new Blog(req.body);
+
+        res.status(200).json({ success: true, data: blog });
+
+        blog.save();
+
+        console.log("CREATED DOCUMENT");
     } catch (error) {
         console.log(error);
         res.json({ error });
     }
 }
-
-
-
