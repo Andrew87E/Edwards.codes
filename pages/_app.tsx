f3ceb50feae6fa2b7c51bf989b6d0575374417f4
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-11-06 13:41:21 -0600

    _app fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import { UserProvider } from '@auth0/nextjs-auth0'
+import { UserProvider } from "@auth0/nextjs-auth0";
 import type { AppProps } from "next/app";
-import "../styles/global.scss";
-import '../build.css'
+import "../styles/global.sass";
+import "../build.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-  <UserProvider>
-  <Component {...pageProps} />
-  </UserProvider>
-  )
+    return (
+        <UserProvider>
+            <Component {...pageProps} />
+        </UserProvider>
+    );
 }
 
 export default MyApp;
